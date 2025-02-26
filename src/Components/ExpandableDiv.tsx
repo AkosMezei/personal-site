3b60d3ce1280240bd8ae3d488f985a6541ecd562
AkosMezei <91083257+AkosMezei@users.noreply.github.com>
2025-02-26 11:18:58 +0200

    Create ExpandableDiv.tsx component and add dummy usage to HomePage.tsx

---
+++ src/Components/ExpandableDiv.tsx
@@ -0,0 +1,104 @@
+﻿//TODO make text orientation left look all right
+//TODO proper content types
+
+import {useState} from "react";
+import {ChevronDown, ChevronUp} from "lucide-react";
+import {AnimatePresence, motion} from "motion/react";
+import {getInitialTheme} from "./BackgroundContext.tsx";
+
+function ExpandableDiv({image = null, title = "Default Title", defaultContent = "Default Content", expandedContent = "Expanded Content", orientation = "left"}: {image?: string | null, title?: string, defaultContent?: any, expandedContent?:any, orientation?: "left" | "right" | "center"}) {
+
+    const [theme] = useState(getInitialTheme);
+    
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    function changeExpanded() {
+        setIsExpanded(!isExpanded);
+    }
+    
+    return (
+            <div onClick={changeExpanded}
+                 className={`w-auto rounded-2xl p-3 hover:bg-white/10 m-3 ${theme === 'dark' ? "bg-white/5" : "bg-black/10"}`}> {/*this is the motherfucker that refuses to update*/}
+                {image} //TODO only so the declaration error leaves me alone
+                {orientation == "left" && (
+                <div className="flex flex-row items-center justify-between">
+                    <h1 className="text-3xl font-bold">{title}</h1>
+                    {isExpanded ? (<ChevronUp/>) : (<ChevronDown/>)}
+                </div>
+                )}
+                
+                {orientation == "right" && (
+                    <div className="flex flex-row items-center justify-between">
+                        {isExpanded ? (<ChevronUp/>) : (<ChevronDown/>)}
+                        <h1 className="text-3xl font-bold">{title}</h1>
+                    </div>
+                )}
+                
+                {orientation == "center" && (
+                    <div className="flex flex-row items-center justify-between">
+                        {isExpanded ? (<ChevronUp/>) : (<ChevronDown/>)}
+                        <h1 className="text-3xl font-bold">{title}</h1>
+                        {isExpanded ? (<ChevronUp/>) : (<ChevronDown/>)}
+                    </div>
+                )}
+                
+                {orientation == "left" && (
+                    <div className="flex flex-row items-center justify-start">
+                        {defaultContent}
+                    </div>
+                )}
+
+                {orientation == "right" && (
+                    <div className="flex flex-row items-center justify-end">
+                        {defaultContent}
+                    </div>
+                )}
+
+                {orientation == "center" && (
+                    <div className="flex flex-row items-center justify-center">
+                        {defaultContent}
+                    </div>
+                )}
+                
+                <AnimatePresence>
+                    {isExpanded && (
+                        <motion.div
+                            initial={{height: 0, opacity: 0}}
+                            animate={{height: "auto", opacity: 1}}
+                            exit={{height: 0, opacity: 0}}
+                            transition={{duration: 0.3, ease: "easeInOut"}}
+                        >
+                            <motion.div initial={{opacity: 0, y: -10}}
+                                        animate={{opacity: 1, y: 0}}
+                                        exit={{y: -15, opacity: 0, transition: {duration: 0.1}}}
+                                        transition={{
+                                            delay: 0.2
+                                        }}>
+                                
+                                {orientation == "left" && (
+                                    <div className="flex flex-col items-start justify-start">
+                                        {expandedContent}
+                                    </div>
+                                )}
+
+                                {orientation == "right" && (
+                                    <div className="flex flex-col items-end justify-end">
+                                        {expandedContent}
+                                    </div>
+                                )}
+
+                                {orientation == "center" && (
+                                    <div className="flex flex-col items-center justify-center">
+                                        {expandedContent}
+                                    </div>
+                                )}
+                                
+                            </motion.div>
+                        </motion.div>
+                    )}
+                </AnimatePresence>
+            </div>
+    )
+}
+
+export default ExpandableDiv;
--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,59 +1,59 @@
 ﻿//TODO refuses to update on theme change, make it work with static bg or make it update
 
-import {useState} from "react";
-import {getInitialTheme} from "./Components/BackgroundContext.tsx";
-import {AnimatePresence, motion} from "motion/react";
-import {ChevronDown, ChevronUp} from "lucide-react";
+import ExpandableDiv from "./Components/ExpandableDiv.tsx";
 
 function HomePage() {
 
-    const [theme] = useState(getInitialTheme);
-
-    const [isExpanded, setIsExpanded] = useState(false);
-
-    function changeExpanded() {
-        setIsExpanded(!isExpanded);
-    }
-
     return (
-        <section className="flex justify-center items-center min-h-screen">
-            <div onClick={changeExpanded}
-                 className={`w-3/5 rounded-2xl p-3 hover:bg-white/10 ${theme === 'dark' ? "bg-white/5" : "bg-black/10"}`}> {/*this is the motherfucker that refuses to update*/}
-                <div className="flex flex-row items-center justify-between">
-                    <h1 className="text-3xl font-bold">Welcome to Homepage</h1>
-                    {isExpanded ? (<ChevronUp/>) : (<ChevronDown/>)}
-                </div>
-                <p> some homepage p content</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-                    voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-                    non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <AnimatePresence>
-                    {isExpanded && (
-                        <motion.div
-                            initial={{height: 0, opacity: 0}}
-                            animate={{height: "auto", opacity: 1}}
-                            exit={{height: 0, opacity: 0}}
-                            transition={{duration: 0.3, ease: "easeInOut"}}
-                        >
-                            <motion.div initial={{opacity: 0, y: -10}}
-                                        animate={{opacity: 1, y: 0}}
-                                        exit={{y: -15, opacity: 0, transition: {duration: 0.1}}}
-                                        transition={{
-                                            delay: 0.2
-                                        }}>
-                                <p> some expando content </p>
-                                <p> Some more expando content woooooooooo </p>
-                                <p> I wonder if</p>
-                                <p> This works with</p>
-                                <p> Way more contents</p>
-                            </motion.div>
-                        </motion.div>
-                    )}
-                </AnimatePresence>
-            </div>
-        </section>
+        <div className="mx-2 py-16">
+            <ExpandableDiv title="About me" orientation="center" defaultContent={
+                <p className="text-center"> Hello! My name is [Your Name], and I'm a passionate programmer with a knack
+                    for
+                    solving complex problems through code. With expertise in various programming languages and
+                    frameworks, I strive to create efficient and innovative solutions. My journey in the tech world has
+                    been fueled by my curiosity and a constant drive to learn and improve. </p>
+            }
+                           expandedContent={
+                               <>
+                                   <p className="mt-3 text-center"> When I'm not coding, you can find me exploring the
+                                       latest tech trends,
+                                       participating
+                                       in hackathons, or enjoying a good book. I'm excited to connect and collaborate
+                                       with
+                                       like-minded individuals who share my enthusiasm for technology. </p>
+                                   <p className="mt-3 text-center text-lg text-red-500/75"> Note: This is a placeholder
+                                       "About Me" section. </p>
+                               </>
+                           }
+            />
+            
+            <ExpandableDiv title={"Left Aligned"}
+                           defaultContent={
+                               <p> This is some left aligned content as a placeholder for testing purposes. </p>
+                           }
+                           expandedContent={
+                               <>
+                                   <p> Expanded left aligned test section</p>
+                                   <p className="mt-3 text-lg text-red-500/75"> Note: This is a placeholder
+                                       section. </p>
+                               </>
+                           }
+            />
+            
+            <ExpandableDiv title={"Right Aligned"}
+                           orientation="right"
+                           defaultContent={
+                               <p> This is some right aligned content as a placeholder for testing purposes. </p>
+                           }
+                           expandedContent={
+                               <>
+                                   <p> Expanded right aligned test section</p>
+                                   <p className="mt-3 text-lg text-red-500/75"> Note: This is a placeholder
+                                       section. </p>
+                               </>
+                           }
+            />
+        </div>
     );
 }
 
