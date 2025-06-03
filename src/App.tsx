95ed64ae10d069f8a1476b726425ba08da1d4e91
AkosMezei <91083257+AkosMezei@users.noreply.github.com>
2025-06-03 15:47:42 +0300

    Add vercel speed insights as injectSpeedInsights.

--- src/App.tsx
+++ src/App.tsx
@@ -77,7 +77,6 @@ function App() {
                     <HomePage/>
                     <Footer/>
                 </motion.main>
-                <SpeedInsights />
             </div>
         </BackgroundContext.Provider>
     );
