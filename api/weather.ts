c8375a8187b4dff9f441cc6844e6ea2583403e8d
AkosMezei <91083257+AkosMezei@users.noreply.github.com>
2025-07-27 22:35:01 +0300

    Enhance error handling in weather API integration, switch to process.env for API key, and add response caching.

--- api/weather.ts
+++ api/weather.ts
@@ -1,19 +1,32 @@
-﻿import axios from 'axios';
+﻿import axios, {isAxiosError} from 'axios';
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default async function handler(request: VercelRequest, response: VercelResponse) {
-    const apiKey = import.meta.env.WEATHER_API_KEY;
+    const apiKey = process.env.WEATHER_API_KEY;
     const location = request.query.location || 'auto:ip';
 
     const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}`;
 
+    if (!apiKey) {
+        console.error("Server Error: WEATHER_API_KEY is not defined.");
+        return response.status(500).json({ error: "Server configuration error." });
+    }
+
     try {
         const weatherResponse = await axios.get(apiUrl);
-
+        response.setHeader('Cache-Control', 's-maxage=600, stale-while-revalidate=1200');
         response.status(200).json(weatherResponse.data);
     }
-    catch (error) {
-        console.error(error);
-        response.status(500).json({ error: "Error fetching weather data. Please try again later." });
+    catch (error:unknown) {
+        //handle axios specific errors
+        if (isAxiosError(error)) {
+            console.error("Axios error fetching from WeatherAPI:", error.response?.data || error.message);
+            return response.status(error.response?.status || 500).json({ error: "Failed to fetch weather data." });
+        }
+        //handle non axios specific errors
+        else {
+            console.error("An unexpected error occurred:", error);
+            return response.status(500).json({ error: "An unexpected server error occurred." });
+        }
     }
 }
