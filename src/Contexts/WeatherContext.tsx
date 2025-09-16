aef14f9ee3692f0324ef58a8ee0ca82275cd8037
AkosMezei <91083257+AkosMezei@users.noreply.github.com>
2025-09-16 16:49:05 +0300

    feat: Enhance WeatherContext and API fallback mechanisms
    - Refactored weather data fetching logic in `WeatherContext` to include geolocation and IP-based fallback.
    - Added support for multiple weather providers.
    - Implemented response normalization and voting mechanism for weather categorization.
    - Updated API endpoint to handle POST requests and dynamically switch between providers based on location availability.

--- src/Contexts/WeatherContext.tsx
+++ src/Contexts/WeatherContext.tsx
@@ -1,7 +1,12 @@
-﻿import { mapWeatherCodeToCategory, WeatherCategory } from '../Utils/weatherUtils';
+﻿import { WeatherCategory } from '../Utils/weatherUtils';
 import {useState, useEffect, useContext, ReactNode} from "react";
 import axios from "axios";
 import React from 'react';
+
+type Location = {
+    lat: number;
+    lon: number;
+}
 
 type WeatherContextType = {
     weatherCategory: WeatherCategory;
@@ -23,39 +28,69 @@ export const WeatherProvider = ({children}:{children:ReactNode}) => {
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(false);
 
+    //function to call the bff
+    const fetchWeatherFromApi = async (location: Location | null) => {
+        try {
+            const response = await axios.post('/api/weather', {location});
+            const category = response.data.category;
+            setWeatherCategory(category);
+            setError(false);
+
+            console.log("response: ", response.data)
+
+            const dataToCache: CachedWeather = {category, timestamp: Date.now()};
+            localStorage.setItem("weather", JSON.stringify(dataToCache));
+        }
+        catch (err) {
+            console.error("Couldn't get weather from API:", err);
+            setError(true);
+        }
+        finally {
+            setIsLoading(false);
+        }
+    }
+
     useEffect(() => {
         const getWeatherData = async () => {
             try {
                 const cachedDataString = localStorage.getItem("weather");
-            if (cachedDataString) {
-                const cachedData: CachedWeather = JSON.parse(cachedDataString)
-                const isCacheFresh = Date.now() - cachedData.timestamp < CACHE_DURATION_MS;
+                if (cachedDataString) {
+                    const cachedData: CachedWeather = JSON.parse(cachedDataString)
+                    const isCacheFresh = Date.now() - cachedData.timestamp < CACHE_DURATION_MS;
 
-                if (isCacheFresh) {
-                    setWeatherCategory(cachedData.category);
-                    setIsLoading(false);
-                    return;
+                    if (isCacheFresh) {
+                        setWeatherCategory(cachedData.category);
+                        setIsLoading(false);
+                        return;
+                    }
                 }
-            }
             } catch (e) {
                 console.error("Couldn't get weather from localStorage:", e);
             }
-            try {
-                const response = await axios.get('/api/weather')
 
-                const code = response.data.current.condition.code;
-                const category = mapWeatherCodeToCategory(code);
-
-                setWeatherCategory(category);
-                setError(false);
-
-                const dataToCache: CachedWeather = {category, timestamp: Date.now()};
-                localStorage.setItem("weather", JSON.stringify(dataToCache));
-            } catch (err) {
-                console.error("Couldn't get weather from API:", err);
-                setError(true);
-            } finally {
-                setIsLoading(false);
+            const cachedLocationString = localStorage.getItem("userLocation");
+            if (cachedLocationString) {
+                const cachedLocation: Location = JSON.parse(cachedLocationString);
+                fetchWeatherFromApi(cachedLocation);
+            } else {
+                if ('geolocation' in navigator) {
+                    navigator.geolocation.getCurrentPosition(
+                        (position) => {
+                            const location: Location = {
+                                lat: position.coords.latitude,
+                                lon: position.coords.longitude,
+                            };
+                            fetchWeatherFromApi(location);
+                        },
+                        (err) => {
+                            console.error("User denied geolocation, falling back to IP", err);
+                            fetchWeatherFromApi(null) //fallback
+                        }
+                    )
+                } else {
+                    console.error("Geolocation not supported, falling back to IP");
+                    fetchWeatherFromApi(null)
+                }
             }
         }
         getWeatherData();
--- src/api/weather.ts
+++ src/api/weather.ts
@@ -1,19 +1,182 @@
 ﻿import axios from 'axios';
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-export default async function handler(request: VercelRequest, response: VercelResponse) {
-    const apiKey = process.env.WEATHER_API_KEY;
-    const location = request.query.location || 'auto:ip';
+type LocationWithCoords = {
+    lat: string,
+    lon: string
+}
 
-    const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}`;
+type CoordProvider = {
+    type: 'coords',
+    name: string,
+    key?: string | null,
+    url: (location: LocationWithCoords, key?: string|null) => string
+}
 
-    try {
-        const weatherResponse = await axios.get(apiUrl);
+type IpProvider = {
+    type: 'ip',
+    name: string,
+    key: string | null,
+    url: (location: string, key?: string|null) => string
+}
 
-        response.status(200).json(weatherResponse.data);
+type WeatherProvider = CoordProvider | IpProvider;
+
+//TODO maybe change lat, lon to number instead of string
+const weatherProviders: WeatherProvider[] = [
+    {
+        type: 'coords',
+        name: "WeatherAPI",
+        url: (location, key) => `https://api.weatherapi.com/v1/current.json?key=${key}&q=${location.lat},${location.lon}`,
+        key: process.env.WEATHER_API_KEY || null
+    },
+
+    {
+        type: 'coords',
+        name: "TomorrowIO",
+        url: (location, key) => `https://api.tomorrow.io/v4/weather/realtime?location=${location.lat}, ${location.lon}&apikey=${key}`,
+        key: process.env.TOMORROWIO_API_KEY || null
+    },
+
+    {
+        type: 'coords',
+        name: "OpenWeatherMap",
+        url: (location, key) => `https://api.openweathermap.org/data/3.0/onecall?lat=${location.lat}&lon=${location.lon}&exclude=minutely,hourly,daily,alerts&appid=${key}`,
+        key: process.env.OPENWEATHERMAP_API_KEY || null
+    },
+
+    {
+        type: 'coords',
+        name: "OpenMeteo",
+        url: (location) => `https://api.open-meteo.com/v1/forecast?latitude=${location.lat}&longitude=${location.lon}&current=weather_code,cloud_cover&forecast_days=1`
+    },
+
+    {
+        type: 'ip',
+        name: "WeatherAPI-FallbackToIP",
+        url: (locationString, key) => `https://api.weatherapi.com/v1/current.json?key=${key}&q=${locationString}`,
+        key: process.env.WEATHER_API_KEY || null
     }
-    catch (error) {
-        console.error(error);
-        response.status(500).json({ error: "Error fetching weather data. Please try again later." });
+]
+
+export default async function handler(request: VercelRequest, response: VercelResponse) {
+    if (request.method !== 'POST') {
+        return response.status(405).json({ error: "Method not allowed." });
+    }
+
+    const clientLocation : LocationWithCoords | null = request.body.location;
+    const location : LocationWithCoords | string = clientLocation || 'auto:ip';
+
+    const hasCoords = typeof location === 'object' && location !== null;
+
+    let apiPromises: Promise<any>[];
+
+    if (hasCoords) {
+        const providersToCall = weatherProviders.filter(provider => provider.type === 'coords')
+        apiPromises = providersToCall.map(provider => {
+            if (!provider.key && provider.name !== 'OpenMeteo')
+                return Promise.reject(new Error(`Provider ${provider.name} requires an API key.`))
+            const requestUrl = provider.url(location, provider.key)
+            return axios.get(requestUrl)
+                .then(axiosResponse => ({
+                    provider: provider.name,
+                    data: axiosResponse.data
+                }))
+        })
+    } else {
+        const providersToCall = weatherProviders.filter(provider => provider.type === 'ip')
+        apiPromises = providersToCall.map(provider => {
+            if (!provider.key)
+                return Promise.reject(new Error(`Provider ${provider.name} requires an API key.`))
+            const requestUrl = provider.url(location, provider.key)
+            return axios.get(requestUrl)
+                .then(axiosResponse => ({
+                    provider: provider.name,
+                    data: axiosResponse.data
+                }))
+        })
+    }
+
+    const results = await Promise.allSettled(apiPromises)
+
+    const successfulResponses = results
+        .filter(result => result.status === 'fulfilled')
+        .map(result => result.value)
+
+    if (successfulResponses.length === 0) {
+        return response.status(500).json({ error: "Error fetching weather data, all providers failed. Please try again later." });
+    }
+
+    //TODO the whole voting system goes here
+    // region Normalization
+
+    const normalizedWeatherCodes = {
+        'clear': 0,
+        'cloudy': 0,
+        'stormy': 0
+    }
+
+    for (const response of successfulResponses) {
+        if (response.provider === 'WeatherAPI' || response.provider === 'WeatherAPI-FallbackToIP'){
+            switch (response.data.current.condition.code) {
+                case 1000:
+                    case 1003: normalizedWeatherCodes.clear++; break;
+                case 1006:
+                    case 1009:
+                        case 1030:
+                            case 1135:
+                                case 1147: normalizedWeatherCodes.cloudy++; break;
+                default: normalizedWeatherCodes.stormy++; break;
+            }
+        }
+        else if (response.provider === 'TomorrowIO'){
+            switch (response.data.values.weatherCode) {
+                case 1000:
+                    case 1100:
+                        case 1101: normalizedWeatherCodes.clear++; break;
+                case 1001:
+                    case 1102:
+                        case 2000:
+                            case 2100: normalizedWeatherCodes.cloudy++; break;
+                default: normalizedWeatherCodes.stormy++; break;
+            }
+        }
+
+        //TODO make these two handle all cases, this is just quickly thrown together to kinda somewhat mostly work
+
+        else if (response.provider === 'OpenWeatherMap'){ //this uses current.clouds as a % of cloud cover, and weather condition codes, so it's more complicated https://openweathermap.org/weather-conditions#Weather-Condition-Codes-2
+            if (response.data.current.clouds <= 25) { //if cloud cover is less than 25%, weather is clear
+                normalizedWeatherCodes.clear++;
+            } else if (response.data.current.weather.id === 800) { //800 is clear, even if cloud cover for some reason is >25%
+                normalizedWeatherCodes.clear++;
+            } else if (response.data.current.weather.id === 500 || response.current.weather.id === 501) { //light or moderate rain
+                normalizedWeatherCodes.cloudy++;
+            } else if (response.data.current.weather.id > 501 && response.data.current.weatherCode < 511) {
+                normalizedWeatherCodes.stormy++;
+            }
+        }
+        else if (response.provider === 'OpenMeteo'){ //also gives a "cloud_cover": as a percentage and a code https://open-meteo.com/en/docs?forecast_days=1&hourly=&current=weather_code,cloud_cover
+            if (response.data.current.cloud_cover <= 25){ //if cloud cover is less than 25%, weather is clear
+                normalizedWeatherCodes.clear++;
+            } else if (response.data.current.weather_code > 50){ //if cover >25%, and code is >50
+                if (response.data.current.weather_code > 60){ //if code is >60, so rain or worse, increase stormy
+                    normalizedWeatherCodes.stormy++;
+                }
+                else{ //if code is between 50-60, so drizzle, set to cloudy
+                    normalizedWeatherCodes.cloudy++;
+                }
+            }
+        }
+    }
+
+    // endregion
+
+    //TODO return response as clear | cloudy | stormy
+    if (normalizedWeatherCodes.clear > normalizedWeatherCodes.cloudy && normalizedWeatherCodes.clear > normalizedWeatherCodes.stormy) {
+        return response.status(200).json({ category: "clear" });
+    } else if (normalizedWeatherCodes.cloudy > normalizedWeatherCodes.clear && normalizedWeatherCodes.cloudy > normalizedWeatherCodes.stormy) {
+        return response.status(200).json({ category: "cloudy" });
+    } else {
+        return response.status(200).json({ category: "stormy" });
     }
 }
