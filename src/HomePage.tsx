f7ad140f400de3d1064c166dc681f2d5d2944962
AkosMezei <91083257+AkosMezei@users.noreply.github.com>
2025-10-07 12:18:07 +0300

    feat: Add text-nowrap for styled components and refactor translation handling
    - Added `text-nowrap` class to styled components for improved text display consistency.
    - Replaced plain translation keys with `<Trans>` component for dynamic translation handling.
    - Minor code cleanup in `HomePage.tsx`.

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,10 +1,4 @@
-﻿//TODO move todos to issues
-//TODO move todos to issues
-
-//TODO if the number of lines is different between languages, the language change is jagged af
-//TODO if on mobile, when opening message sender, jump to bottom so keyboard doesn't hide input 
-//TODO fix centered titles inside projects not being centered on mobile
-//TODO add hrefs to tech stack in about me
+﻿//TODO if the number of lines is different between languages, the language change is jagged af
 
 import ExpandableDiv from "./Components/ExpandableDiv.tsx";
 import MessageBox from "./Components/MessageBox.tsx";
@@ -17,39 +11,41 @@ function HomePage() {
     const { t } = useTranslation();
 
     const styledComponents = {
-        reactLink: <a href="https://react.dev" target="_blank" className="hover:text-react font-bold" />,
-        typeScriptLink: <a href="https://www.typescriptlang.org" target="_blank" className="hover:text-typescript font-bold" />,
-        dotnetLink: <a href="https://dotnet.microsoft.com/" target="_blank" className="hover:bg-gradient-to-bl hover:from-dotNetStart hover:from-45% hover:to-dotNetEnd hover:to-80% hover:text-transparent hover:bg-clip-text font-bold" />,
-        viteLink: <a href="https://vite.dev" target="_blank" className="hover:bg-gradient-to-br hover:from-viteStart hover:from-45% hover:via-viteMiddle hover:via-50% hover:to-viteEnd hover:to-80% hover:text-transparent hover:bg-clip-text font-bold" />,
-        tailwindLink: <a href="https://tailwindcss.com" target="_blank" className="hover:text-tailwind font-bold" />,
-        framerMotionLink: <a href="https://motion.dev" target="_blank" className="hover:text-framerMotion font-bold" />,
-        vercelLink: <a href="https://vercel.com/" target="_blank" className="hover:text-black/90 font-bold" />,
-        mongoDBLink: <a href="https://www.mongodb.com" target="_blank" className="hover:text-mongoDB font-bold" />,
-        dockerLink: <a href="https://www.docker.com" target="_blank" className="hover:text-docker font-bold" />,
-        digitalOceanLink: <a href="https://www.digitalocean.com" target="_blank" className="hover:text-digitalOcean font-bold" />,
-        javascriptLink: <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" className="hover:text-javascript font-bold" />,
-        pythonLink: <a href="https://www.python.org" target="_blank" className="hover:text-python font-bold" />,
-        fastAPILink: <a href="https://fastapi.tiangolo.com" target="_blank" className="hover:text-fastAPI font-bold" />,
-        flaskLink: <a href="https://flask.palletsprojects.com" target="_blank" className="hover:text-black dark:hover:text-white font-bold" />,
-        postgresLink: <a href="https://www.postgresql.org" target="_blank" className="hover:text-postgres font-bold" />,
-        plotlyLink: <a href="https://plotly.com" target="_blank" className="hover:text-plotly font-bold" />,
-        threejsLink: <a href="https://threejs.org" target="_blank" className="hover:text-black dark:hover:text-white font-bold" />,
-        geminiLink: <a href="https://ai.google.dev/gemini-api" target="_blank" className="hover:bg-gradient-to-r hover:from-geminiStart hover:to-geminiEnd hover:text-transparent hover:bg-clip-text font-bold" />,
-        csharpLink: <a href="https://learn.microsoft.com/en-us/dotnet/csharp/" target="_blank" className="hover:text-dotNetStart font-bold" />,
-        matlabLink: <a href="https://www.mathworks.com/products/matlab.html" target="_blank" className="hover:text-matlab font-bold" />,
-        SPALink: <a href="https://developer.mozilla.org/en-US/docs/Glossary/SPA" target="_blank" className="font-semibold italic hover:underline" />,
-        playwrightLink: <a href="https://playwright.dev" target="_blank" className="hover:text-playwright font-bold" />,
-        name: <strong className="font-semibold text-gray-900 dark:text-gray-100" />,
+        reactLink: <a href="https://react.dev" target="_blank" className="hover:text-react font-bold text-nowrap" />,
+        typeScriptLink: <a href="https://www.typescriptlang.org" target="_blank" className="hover:text-typescript font-bold text-nowrap" />,
+        dotnetLink: <a href="https://dotnet.microsoft.com/" target="_blank" className="hover:bg-gradient-to-bl hover:from-dotNetStart hover:from-45% hover:to-dotNetEnd hover:to-80% hover:text-transparent hover:bg-clip-text font-bold text-nowrap" />,
+        viteLink: <a href="https://vite.dev" target="_blank" className="hover:bg-gradient-to-br hover:from-viteStart hover:from-45% hover:via-viteMiddle hover:via-50% hover:to-viteEnd hover:to-80% hover:text-transparent hover:bg-clip-text font-bold text-nowrap" />,
+        tailwindLink: <a href="https://tailwindcss.com" target="_blank" className="hover:text-tailwind font-bold text-nowrap" />,
+        framerMotionLink: <a href="https://motion.dev" target="_blank" className="hover:text-framerMotion font-bold text-nowrap" />,
+        vercelLink: <a href="https://vercel.com/" target="_blank" className="hover:text-black/90 font-bold text-nowrap" />,
+        mongoDBLink: <a href="https://www.mongodb.com" target="_blank" className="hover:text-mongoDB font-bold text-nowrap" />,
+        dockerLink: <a href="https://www.docker.com" target="_blank" className="hover:text-docker font-bold text-nowrap" />,
+        digitalOceanLink: <a href="https://www.digitalocean.com" target="_blank" className="hover:text-digitalOcean font-bold text-nowrap" />,
+        javascriptLink: <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" className="hover:text-javascript font-bold text-nowrap" />,
+        pythonLink: <a href="https://www.python.org" target="_blank" className="hover:text-python font-bold text-nowrap" />,
+        fastAPILink: <a href="https://fastapi.tiangolo.com" target="_blank" className="hover:text-fastAPI font-bold text-nowrap" />,
+        flaskLink: <a href="https://flask.palletsprojects.com" target="_blank" className="hover:text-black dark:hover:text-white font-bold text-nowrap" />,
+        postgresLink: <a href="https://www.postgresql.org" target="_blank" className="hover:text-postgres font-bold text-nowrap" />,
+        plotlyLink: <a href="https://plotly.com" target="_blank" className="hover:text-plotly font-bold text-nowrap" />,
+        threejsLink: <a href="https://threejs.org" target="_blank" className="hover:text-black dark:hover:text-white font-bold text-nowrap" />,
+        geminiLink: <a href="https://ai.google.dev/gemini-api" target="_blank" className="hover:bg-gradient-to-r hover:from-geminiStart hover:to-geminiEnd hover:text-transparent hover:bg-clip-text font-bold text-nowrap" />,
+        csharpLink: <a href="https://learn.microsoft.com/en-us/dotnet/csharp/" target="_blank" className="hover:text-dotNetStart font-bold text-nowrap" />,
+        matlabLink: <a href="https://www.mathworks.com/products/matlab.html" target="_blank" className="hover:text-matlab font-bold text-nowrap" />,
+        SPALink: <a href="https://developer.mozilla.org/en-US/docs/Glossary/SPA" target="_blank" className="font-semibold italic hover:underline text-nowrap" />,
+        playwrightLink: <a href="https://playwright.dev" target="_blank" className="hover:text-playwright font-bold text-nowrap" />,
+        umbracoLink: <a href="https://umbraco.com" target="_blank" className="hover:text-umbraco font-bold text-nowrap" />,
+        name: <strong className="font-semibold text-gray-900 dark:text-slate-100" />,
         bold: <strong />,
-        subtitle: <strong className="font-semibold text-gray-900 dark:text-gray-100 mr-2" />,
-        code: <code className="font-mono bg-slate-200 dark:bg-slate-700/80 text-sky-600 dark:text-sky-300 px-2 py-1 rounded-md text-sm" />,
+        subtitle: <strong className="font-semibold text-gray-900 dark:text-slate-100 mr-2" />,
+        code: <code className="font-mono text-nowrap bg-slate-200 dark:bg-slate-700/80 text-sky-600 dark:text-sky-300 px-1 py-0.5 rounded-md text-sm" />,
     }
-
+    //TODO rewrite to either use theme or make dark: in css use theme
     return (
         <div className="mx-2 py-16 lg:w-3/5 ml-auto mr-auto">
             <motion.div layout transition={{layout: {duration: 0.5, ease: "easeInOut"}}}>
                 <ExpandableDiv title={t('aboutMe.title')}
                                orientation="center"
+                               isSectionBreak={true}
                                defaultContent={
                                    <p className="text-center">
                                        <Trans i18nKey="aboutMe.description" components={styledComponents}/>
@@ -177,10 +173,10 @@ function HomePage() {
                             expandedContent={
                                 <ul>
                                     <li>
-                                        {t('experience.internship.bullet_1')}
+                                        <Trans i18nKey="experience.internship.bullet_1" components={styledComponents}/>
                                     </li>
                                     <li>
-                                        {t('experience.internship.bullet_2')}
+                                        <Trans i18nKey="experience.internship.bullet_2" components={styledComponents}/>
                                     </li>
                                     <li>
                                         {t('experience.internship.bullet_3')}
