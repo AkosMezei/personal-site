f30b6bb4622a76d2fb0a3bfdf2fd5b33eee72cad
AkosMezei <91083257+AkosMezei@users.noreply.github.com>
2025-11-05 15:39:40 +0200

    feat: Add ExpandableImage component with modal and Framer Motion integration
    - Introduced `ExpandableImage` component for interactive image expansion with modal overlay.
    - Utilized Framer Motion for smooth animations.
    - Managed body overflow to prevent content shift during modal open.

---
+++ src/Components/ExpandableImage.tsx
@@ -0,0 +1,62 @@
+﻿import {AnimatePresence, motion} from "motion/react";
+import {useEffect, useState} from "react";
+import {createPortal} from "react-dom";
+
+type ExpandableImageProps = {
+    src:string,
+    alt:string,
+    maxHeight?:string,
+    maxWidth?:string,
+}
+
+export const ExpandableImage = ({src, alt, }:ExpandableImageProps) => {
+    const [isOpened, setIsOpened] = useState<boolean>(false);
+
+    useEffect(() => {
+        if (isOpened) {
+            const scrollBarWidth = window.innerWidth - document.body.clientWidth;
+            document.body.style.paddingRight = `${scrollBarWidth}px`; //add scroll bar width to padding to body when modal is open, to prevent content shift
+            document.body.style.overflow = "hidden";
+        } else {
+            document.body.style.overflow = "unset";
+            document.body.style.paddingRight = "0px";
+        }
+
+        return () => {
+            document.body.style.overflow = "unset";
+            document.body.style.paddingRight = "0px";
+        }
+
+    }, [isOpened]);
+
+    return (
+        <>
+            <motion.img layout whileHover={{scale: 1.02}}
+                        animate={{ opacity: isOpened ? 0 : 1 }} //hide the image when the modal is opened
+                        transition={{ opacity: { duration: 0.2 } }}
+                        className={`pt-1 pb-1 rounded-2xl mx-auto object-cover max-w-[80%] max-h-64`}
+                        alt={alt}
+                        src={src}
+                        layoutId={src}
+                        onClick={() => setIsOpened(true)}
+            />
+                {
+                    createPortal(
+                        <AnimatePresence>
+                            {isOpened &&
+                                <motion.div initial={{ opacity: 0 }}
+                                    animate={{ opacity: 1 }}
+                                    exit={{ opacity: 0 }}
+                                    className="fixed inset-0 bg-black/50 backdrop-blur-md flex items-center justify-center z-50" onClick={() => setIsOpened(false)}
+                                >
+                                    <motion.img layoutId={src} transition={{duration:0.5}} onClick={(e) => e.stopPropagation()}
+                                                className="max-h-[90vh] max-w-[90vw] rounded-2xl object-contain" src={src}/>
+                                </motion.div>
+                            }
+                        </AnimatePresence>,
+                    document.body
+                    )
+                }
+        </>
+    );
+};
