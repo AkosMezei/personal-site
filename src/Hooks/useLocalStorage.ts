bac845b723b0dac34f9daf52bdb62496c4355918
AkosMezei <91083257+AkosMezei@users.noreply.github.com>
2025-11-07 14:36:25 +0200

    refactor: Simplify ThemeSettingsContext with `useLocalStorage` hook
    - Replaced redundant `useState` and `useEffect` logic for local storage management with a reusable `useLocalStorage` hook.
    - Removed initialization functions and repetitive error handling by centralizing logic in the `useLocalStorage` implementation.
    - Added `useLocalStorage` hook to handle state persistence and synchronization.

---
+++ src/Hooks/useLocalStorage.ts
@@ -0,0 +1,32 @@
+﻿import {useEffect, useState} from "react";
+
+
+function getStoredValue<T>(key:string, initialValue:T):T{
+    if (typeof window === 'undefined') {
+        return initialValue;
+    }
+
+    try {
+        const item = localStorage.getItem(key);
+        return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+        console.error(error);
+        return initialValue;
+    }
+}
+
+export const useLocalStorage = <T>(key:string, initialValue:T) => {
+    const [storedValue, setStoredValue] = useState<T>(()=>{
+        return getStoredValue(key, initialValue);
+    })
+
+    useEffect(() => {
+        try {
+            window.localStorage.setItem(key, JSON.stringify(storedValue));
+        } catch (error) {
+            console.error(error);
+        }
+    }, [key, storedValue]);
+
+    return [storedValue, setStoredValue] as const;
+}
