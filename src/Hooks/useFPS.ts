4dc4bd1f3c9a8bd74b36b3837a1c77641c586d8b
AkosMezei <91083257+AkosMezei@users.noreply.github.com>
2025-11-10 21:30:15 +0200

    feat(Hooks): add `useFPS` custom hook for frame rate tracking
    - Introduced a reusable `useFPS` hook to monitor and calculate the average frames per second (FPS) using `requestAnimationFrame`.

---
+++ src/Hooks/useFPS.ts
@@ -0,0 +1,36 @@
+﻿import {useEffect, useRef, useState} from "react";
+
+const FRAME_SAMPLES = 60 // number of frames to average over
+
+export const useFPS = () => {
+    const [fps, setFps] = useState(60)
+    const frameTimes = useRef<number[]>([])
+    const lastTimestamp = useRef(performance.now())
+
+    useEffect(() => {
+        let frameId: number
+
+        const measure = (timestamp:number) => {
+            const deltaTime = timestamp - lastTimestamp.current
+            lastTimestamp.current = timestamp
+
+            frameTimes.current.push(deltaTime) //add new frame time
+            if (frameTimes.current.length >= FRAME_SAMPLES) { //if the number of samples is larger than what we want
+                frameTimes.current.shift()
+            }
+
+            //calculate the avg delta time
+            const averageDelta = frameTimes.current.reduce((a, b) => a + b, 0) / frameTimes.current.length
+            const currentFps = 1000 / averageDelta
+            setFps(currentFps)
+
+            frameId = requestAnimationFrame(measure)
+        }
+
+        frameId = requestAnimationFrame(measure)
+
+        return () => cancelAnimationFrame(frameId)
+    }, []);
+
+    return fps;
+}
