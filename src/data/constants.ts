02cc13c87d760156f41a00521fa0c68c6ef34262
AkosMezei <91083257+AkosMezei@users.noreply.github.com>
2025-11-13 09:05:18 +0200

    docs(data): add JSDoc comment for constants file

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,1 +1,4 @@
-﻿export const MOBILE_BREAKPOINT_PX = 765
+﻿/**
+ * @File - file to store constants that are used at multiple points and should be easily changeable for fine tuning
+ */
+export const MOBILE_BREAKPOINT_PX = 765
