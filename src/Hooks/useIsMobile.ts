d4cc3694134346845379828e2ab402a95aee3256
AkosMezei <91083257+AkosMezei@users.noreply.github.com>
2025-11-13 09:27:48 +0200

    personal-site-90 docs(hooks): Document the useIsMobile hook

--- src/Hooks/useIsMobile.ts
+++ src/Hooks/useIsMobile.ts
@@ -1,6 +1,16 @@
-﻿import {MOBILE_BREAKPOINT_PX} from "../data/constants.ts";
+﻿/**
+ * @File - simple hook that checks if current viewport width corresponds to a mobile size
+ */
+
+import {MOBILE_BREAKPOINT_PX} from "../data/constants.ts";
 import {useEffect, useState} from "react";
 
+/**
+ * A custom React hook to determine if the user's device screen width is considered mobile.
+ * The determination is based on a predefined breakpoint value.
+ *
+ * @return {boolean} Returns a boolean indicating whether the viewport width is considered mobile.
+ */
 export function useIsMobile() {
     const [isMobile, setIsMobile] = useState<boolean>(false);
 
