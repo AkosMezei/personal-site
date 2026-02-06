4f5eb8e2a5da1309b8726068572be24ee3d2910e
AkosMezei <91083257+AkosMezei@users.noreply.github.com>
2026-02-06 14:03:05 +0200

    feat(components): introduce `Blinker` component with size and color variants
    - Added `Blinker` functional component for visual blinking effects.
    - Included support for size (2, 3, 4) and color (`sky`, `green`, `red`, `yellow`) props.
    - Utilized Tailwind classes for styling and animations.

---
+++ src/Components/FX/Blinker.tsx
@@ -0,0 +1,34 @@
+﻿type BlinkerProps = {
+    size: 2 | 3 | 4;
+    color: 'sky' | 'green' | 'red' | 'yellow';
+    className?: string;
+};
+
+const sizeClasses = {
+    2: 'h-2 w-2',
+    3: 'h-3 w-3',
+    4: 'h-4 w-4',
+};
+
+const colorClasses = {
+    sky: 'bg-sky-500',
+    green: 'bg-green-500',
+    red: 'bg-red-500',
+    yellow: 'bg-yellow-500',
+};
+
+export const Blinker = ({size, color, className = ''}:BlinkerProps) => {
+    const classes = `
+    ${sizeClasses[size]} 
+    ${colorClasses[color]}
+    animate-blink-blinker 
+    rounded-full
+    absolute 
+    top-1.5 left-1.5
+    ${className}
+  `;
+
+    return (
+        <div className={classes}></div>
+    );
+};
