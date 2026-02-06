7484a76701c4e267807b68241e29e66f7213fc5f
AkosMezei <91083257+AkosMezei@users.noreply.github.com>
2026-02-06 14:04:47 +0200

    feat(utils): add `FlowInteraction` component for dynamic viewport-based active node selection
    - Introduced `FlowInteraction` component using `@xyflow/react`.
    - Dynamically identifies and sets active nodes based on the viewport's center position and zoom level.
    - Integrated tolerance adjustments for responsive node filtering.

---
+++ src/Utils/FlowInteraction.tsx
@@ -0,0 +1,37 @@
+﻿import { useReactFlow, useOnViewportChange } from '@xyflow/react';
+import { useSetActiveNodeIds } from "../Contexts/ActiveNodeContext.tsx";
+
+export const FlowInteraction = ({containerHeight = 600, containerWidth = 800}) => {
+    const {getNodes} = useReactFlow();
+    const setActiveIds = useSetActiveNodeIds();
+
+    useOnViewportChange({
+        onChange: (viewport) => {
+            const middleY = containerHeight / 2;
+            const middleX = containerWidth / 2;
+
+            const targetWorldY = (middleY - viewport.y) / viewport.zoom;
+            const targetWorldX = (middleX - viewport.x) / viewport.zoom;
+
+            const nodes = getNodes();
+
+            const rowNodes = nodes.filter(node => {
+                const distanceY = Math.abs(node.position.y - targetWorldY);
+                return distanceY < 150;
+            })
+            const baseToleranceX = viewport.zoom < 1 ? 600 : 200;
+
+            const toleranceX = baseToleranceX / viewport.zoom;
+
+            const activeIds = rowNodes.filter(node => {
+                const distanceX = Math.abs(node.position.x - targetWorldX);
+                return distanceX < toleranceX;
+            })
+                .map(node => node.id);
+
+            setActiveIds(activeIds);
+        }
+    })
+
+    return null
+};
