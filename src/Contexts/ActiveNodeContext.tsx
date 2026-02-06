3a9cf1038b8f4a95aada8d4355c9e709929633ff
AkosMezei <91083257+AkosMezei@users.noreply.github.com>
2026-02-06 14:04:39 +0200

    feat(contexts): add `ActiveNodeContext` for managing active node states
    - Introduced `ActiveNodeProvider` to provide context for active node IDs.
    - Added `useActiveNodeIds` and `useSetActiveNodeIds` hooks for accessing and updating active nodes.

---
+++ src/Contexts/ActiveNodeContext.tsx
@@ -0,0 +1,24 @@
+﻿import {createContext, ReactNode, useContext, useState} from 'react';
+
+const ActiveNodeContext = createContext<string[]>([]);
+const SetActiveNodeContext = createContext<((ids: string[]) => void) | null>(null)
+
+export const ActiveNodeProvider = ({ children }:{children: ReactNode}) => {
+    const [activeIds, setActiveIds] = useState<string[]>(['AddNewItem']);
+    return (
+        <ActiveNodeContext.Provider value={activeIds}>
+            <SetActiveNodeContext.Provider value={setActiveIds}>
+                {children}
+            </SetActiveNodeContext.Provider>
+        </ActiveNodeContext.Provider>
+    );
+};
+
+export const useActiveNodeIds = () => useContext(ActiveNodeContext);
+export const useSetActiveNodeIds = () => {
+    const context = useContext(SetActiveNodeContext)
+    if (context === null) {
+        throw new Error('useSetActiveNodeIds must be used within a ActiveNodeProvider')
+    }
+    return context;
+};
