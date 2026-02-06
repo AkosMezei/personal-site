989780f913eee9ea46eae6c60991034667605e0f
AkosMezei <91083257+AkosMezei@users.noreply.github.com>
2026-02-06 14:03:30 +0200

    feat(nodes): add `AiNode` component with active state animations and tooltip
    - Introduced `AiNode` component leveraging `@xyflow/react`.
    - Added active state animations using `motion` for scale, opacity, and blur effects.
    - Integrated `InfoTooltip` for displaying node description with smooth transitions.
    - Included interactive handles for source and target connections.

---
+++ src/Components/Nodes/AiNode.tsx
@@ -0,0 +1,64 @@
+﻿import { Handle, Position} from '@xyflow/react'
+import {Blinker} from "../FX/Blinker.tsx";
+import {useActiveNodeIds} from "../../Contexts/ActiveNodeContext.tsx";
+import { motion } from 'motion/react';
+import {nodeProps} from "../Sections/Flowchart.tsx";
+
+const InfoTooltip = ({text, isActive, side = 'right'} :{text:string, isActive:boolean, side:string}) => {
+    const positionClass = side === 'left'
+        ? 'right-[100%] mr-6 origin-right'
+        : 'left-[100%] ml-6 origin-left'
+
+    return (
+        <motion.div
+            animate={{
+                scale: isActive ? 1 : 0,
+                opacity: isActive ? 1 : 0,
+                filter: isActive ? 'blur(0px)' : 'blur(2px)',
+            }}
+            transition={{ duration: 0.3 }}
+
+            className={`absolute ${positionClass} w-64 bg-slate-800/90 border border-slate-600 rounded-lg shadow-xl z-50 pointer-events-none`}>
+            <div className="m-3">
+                {text}
+            </div>
+        </motion.div>
+    )
+}
+
+export const AiNode = ({id, data}:nodeProps) => {
+    const activeIds = useActiveNodeIds();
+    const isActive = activeIds.includes(id);
+
+    return (
+        <div className="flex items-center">
+        <motion.div
+            animate={{
+                scale: isActive ? 1.1 : 0.9,
+                opacity: isActive ? 1 : 0.4,
+                filter: isActive ? 'blur(0px)' : 'blur(2px)',
+            }}
+            transition={{ duration: 0.3 }}
+
+            className="px-4 py-2 shadow-md rounded-md bg-white border-2 border-stone-400 max-w-60">
+            <Blinker size={2} color="sky"/>
+            <div className="text-sky-400 break-words text-center">
+                {data.label}
+            </div>
+
+            <Handle type="target" position={Position.Top} />
+            <Handle type="target" position={Position.Left} id="left"/>
+            <Handle type="target" position={Position.Right} id="right"/>
+
+            <Handle type={"source"} position={Position.Bottom} />
+        </motion.div>
+            {data.description &&
+                <InfoTooltip
+                    text={data.description}
+                    isActive={isActive}
+                    side={data.tooltipSide || 'right'}
+                />
+            }
+        </div>
+    );
+};
